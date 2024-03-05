e83c275fd3874e7f433655f3b31da8b3da2c6e99
Nikesh Milind Patil <nikeshmpatil248@gmail.com>
2024-03-05 06:03:13 +0000

    first route setup

--- app.py
+++ app.py
@@ -1,11 +1,14 @@
-from flask import Flask 
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def Hello_world():
-    return "Hello world"
+    return render_template('index.html')
 
+@app.route('/home')
+def Home():
+    return "this is home page"
 
 if __name__ == '__main__':
     app.run(debug = True)
